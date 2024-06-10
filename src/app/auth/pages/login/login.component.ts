ca1ee50fedea57646bf016610643e9ec3ab4267a
Fernando García (Incuerd0) <42920098+fernandoG494@users.noreply.github.com>
2024-06-10 00:15:30 -0600

    basic page mlogin implemetation

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
 import { LoginRegisterContainerComponent } from '../../../shared/layout/login-register-container/login-register-container.component';
 
 @Component({
-  imports: [LoginRegisterContainerComponent],
+  imports: [LoginRegisterContainerComponent, MatTabsModule],
   selector: 'auth-login',
   standalone: true,
   styleUrl: './login.component.scss',
--- src/app/shared/layout/login-register-container/login-register-container.component.ts
+++ src/app/shared/layout/login-register-container/login-register-container.component.ts
@@ -1,8 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @Component({
-  imports: [CommonModule],
+  encapsulation: ViewEncapsulation.None,
+  imports: [CommonModule, MatGridListModule, MatTabsModule],
   selector: 'login-register-container',
   standalone: true,
   styleUrl: './login-register-container.component.scss',
